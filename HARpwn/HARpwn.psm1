931c73a2708bb94d4a77944e81e0d7ae84e3a481
Christian Ritter <h.christian.ritter@gmail.com>
2023-11-02 14:51:01 +0100

    Initial module files and updated README
    This commit includes the initial PSM1 and PSD1 files, providing the foundation for the "HARpwn" module. Additionally, it updates the README.md with detailed usage instructions for installing, extracting, and sanitizing HARTokens. Users can now easily utilize the module's capabilities.

---
+++ HARpwn/HARpwn.psm1
@@ -0,0 +1,103 @@
+function Get-HARToken {
+    <#
+    .SYNOPSIS
+    Extracts HARTokens from an HTTP Archive (HAR) file.
+
+    .DESCRIPTION
+    The Get-HARToken function extracts HARTokens from an HTTP Archive (HAR) file, specifically designed for the 'Graph' token type. It processes the HAR file, filters for 'Bearer' tokens with 'graph' scope, and provides information about the tokens, including scopes, access tokens, refresh tokens, and expiration times.
+
+    .PARAMETER type
+    Specifies the type of token to extract. Currently, only 'Graph' is supported.
+
+    .PARAMETER filePath
+    Specifies the path to the HAR file from which tokens will be extracted.
+
+    .EXAMPLE
+    Get-HARToken -type Graph -filePath 'C:\path\to\example.har'
+
+    This example extracts 'Graph' tokens from the specified HAR file and provides details about the tokens.
+
+    .NOTES
+    File paths must be specified as absolute paths, and the HAR file should be in JSON format.
+    #>
+    [CmdletBinding()]
+    param (
+        [ValidateSet('Graph')]
+        $type,
+        [string]
+        $filePath
+    )
+    
+    begin {
+        $Har = Get-Content -Path $filePath | ConvertFrom-Json
+        $Responses = $har.log.entries.response.content.text
+    }
+    
+    process {
+        switch($Type){
+            'Graph'{
+                foreach($Response in $Responses){
+                    try{$Response | ConvertFrom-Json -ErrorAction SilentlyContinue | Where-Object { $_.token_type -eq 'Bearer' -and $_.Scope.contains("graph")} | ForEach-Object{
+                        [PSCustomObject]@{
+                            Scopes = -split$_.scope | Where-Object{$_.contains("graph")}|ForEach-Object{([uri]$_).absolutePath-replace"/"}
+                            Token = $_.access_token
+                            RefreshToken = $_.refresh_token
+                            ExpiresIn = $_.Expires_In
+                        }
+                    }}catch{}
+                }
+            }
+        }
+    }
+    
+    end {
+        
+    }
+}
+
+function Remove-HARToken {
+    <#
+    .SYNOPSIS
+    Sanitizes HARTokens in an HTTP Archive (HAR) file.
+
+    .DESCRIPTION
+    The Remove-HARToken function sanitizes HARTokens in an HTTP Archive (HAR) file by replacing the tokens with a specified word (default is "Removed"). It creates a sanitized version of the HAR file without sensitive tokens, allowing for secure sharing or analysis.
+
+    .PARAMETER type
+    Specifies the type of token to sanitize. Currently, only 'Graph' is supported.
+
+    .PARAMETER filePath
+    Specifies the path to the HAR file to sanitize.
+
+    .PARAMETER SantinzeWord
+    Specifies the word to replace the sensitive tokens with. The default value is "Removed."
+
+    .EXAMPLE
+    Remove-HARToken -type Graph -filePath 'C:\path\to\example.har' -SantinzeWord 'Redacted'
+
+    This example sanitizes 'Graph' tokens in the specified HAR file, replacing them with the word 'Redacted' and creates a sanitized version of the file.
+
+    .NOTES
+    File paths must be specified as absolute paths, and the HAR file should be in JSON format.
+    #>
+    [CmdletBinding()]
+    param (
+        [ValidateSet('Graph')]
+        $type,
+        [string]
+        $filePath,
+        $SantinzeWord ="Removed"
+    )
+    
+    process {
+        $NewFile = Get-ChildItem -file -Path $filePath 
+        $HarContent = Get-Content -Path $filePath
+        $NewFileName = $($NewFile.FullName -replace ".har","_Santinzed.har")
+        
+        (Get-HARToken -type $Type -filePath $filePath).Token.ForEach({
+            $HarContent = $HarContent -Replace $_,$SantinzeWord
+        })
+        $HarContent | Out-File -FilePath $NewFileName -Force
+        Write-Verbose -Message "New File: $NewFileName"
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # HARpwn
+
 ## HARToken Extraction and Sanitization Module
 
 HARpwn is a PowerShell module designed to streamline the extraction and sanitization of HARTokens from HTTP Archive (HAR) files. Simplify the process of retrieving valuable data and ensure your HAR files are safe and secure with HARpwn, where HARTokens meet 'pwnage' with a touch of humor.
 ![HARpwn-Logo](https://github.com/HCRitter/HARpwn/blob/main/HARpwn-Logo.jpg?raw=true?raw=true)
+
 ## use cases
+
 - Secure Sharing of HAR Files: Before sharing HAR files with third parties for debugging or analysis, users can employ HARpwn to sanitize the files by replacing tokens with placeholders, ensuring sensitive information remains protected.
 - Educational Purposes: Students and educators can use HARpwn for hands-on learning and teaching about web traffic analysis, security, and token extraction.
+
+## usage
+
+1. Installation
+    - download or clone this repo
+    - Import the module into your PowerShell session using the following command:
+
+    ```powershell
+    Import-Module .\HARpwn.psm1
+    ```
+
+2. Extract HARTokens
+    - Use Get-HARToken to extract HARTokens from a HAR file.
+    - Example
+  
+    ```powershell
+    Get-HARToken -type Graph -filePath 'C:\path\to\example.har'
+    ```
+
+3. Sanitize HARTokens
+   - Use Remove-HARToken to sanitize a HAR file by replacing sensitive tokens with a specified word (default is "Removed").
+   - Example
+  
+  ```powershell
+  Remove-HARToken -type Graph -filePath 'C:\path\to\example.har' -SantinzeWord 'Redacted'
+  ```
+  
